b90cdded82309f71ac9b6e5a7c22f6b6b9a7f2e4
Tommy Pranoto <tommyprn8@gmail.com>
2020-12-09 18:27:43 +0700

    test YOT

--- src/App.js
+++ src/App.js
@@ -1,23 +1,184 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
+import _ from "lodash";
+import { v4 } from "uuid";
+
+const item = {
+  id: v4(),
+  name: "Clean the house",
+};
+
+const item2 = {
+  id: v4(),
+  name: "Wash the car",
+};
 
 function App() {
+  const [text, setText] = useState("");
+  const [state, setState] = useState({
+    todo: {
+      title: "todo",
+      items: [item, item2],
+    },
+    doing: {
+      title: "in progress",
+      items: [],
+    },
+    done: {
+      title: "done",
+      items: [],
+    },
+  });
+
+  const handleDragEnd = ({ destination, source }) => {
+    if (!destination) {
+      return;
+    }
+
+    if (
+      destination.index === source.index &&
+      destination.droppableId === source.droppableId
+    ) {
+      return;
+    }
+
+    const itemCopy = { ...state[source.droppableId].items[source.index] };
+
+    setState((prev) => {
+      prev = { ...prev };
+      prev[source.droppableId].items.splice(source.index, 1);
+
+      prev[destination.droppableId].items.splice(
+        destination.index,
+        0,
+        itemCopy
+      );
+
+      return prev;
+    });
+  };
+
+  const addItem = () => {
+    setState((prev) => {
+      return {
+        ...prev,
+        todo: {
+          title: "Todo",
+          items: [
+            {
+              id: v4(),
+              name: text,
+            },
+            ...prev.todo.items,
+          ],
+        },
+      };
+    });
+
+    setText("");
+  };
+
+  const delItem = (id, event) => {
+    setState((prev) => {
+      let todo;
+      let doing;
+      let done;
+
+      todo = prev.todo.items.filter((item) => item.id !== id);
+      doing = prev.doing.items.filter((item) => item.id !== id);
+      done = prev.done.items.filter((item) => item.id !== id);
+
+      prev.todo.items = todo;
+      prev.doing.items = doing;
+      prev.done.items = done;
+
+      return { ...prev };
+    });
+    setText("");
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="AddBar">
+        <input
+          className="Inputs"
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
+        <button onClick={addItem} className="ButtonAdd">
+          Add
+        </button>
+      </div>
+      <DragDropContext onDragEnd={handleDragEnd}>
+        <div className="CardContainer">
+          {_.map(state, (data, key) => {
+            return (
+              <div
+                key={key}
+                className="CardHolder"
+                style={
+                  data.title === "todo"
+                    ? { backgroundColor: "#fffd82" }
+                    : data.title === "done"
+                    ? { backgroundColor: "#ff9b71" }
+                    : { backgroundColor: "#dbf4a7" }
+                }
+              >
+                <p className="HeaderTitle">{data.title}</p>
+                <Droppable droppableId={key}>
+                  {(provided, snapshot) => {
+                    return (
+                      <div
+                        ref={provided.innerRef}
+                        {...provided.droppableProps}
+                        className={"droppable-col"}
+                      >
+                        {data.items.map((el, index) => {
+                          return (
+                            <Draggable
+                              key={el.id}
+                              index={index}
+                              draggableId={el.id}
+                            >
+                              {(provided, snapshot) => {
+                                return (
+                                  <div
+                                    className={`item ${
+                                      snapshot.isDragging && "dragging"
+                                    }`}
+                                    ref={provided.innerRef}
+                                    {...provided.draggableProps}
+                                    {...provided.dragHandleProps}
+                                  >
+                                    <input
+                                      type="checkbox"
+                                      className="Checkbox"
+                                    />
+                                    <div>{el.name}</div>
+                                    <button
+                                      className="DeleteButton"
+                                      onClick={(e) => delItem(el.id)}
+                                    >
+                                      X
+                                    </button>
+                                  </div>
+                                );
+                              }}
+                            </Draggable>
+                          );
+                        })}
+                        {provided.placeholder}
+                      </div>
+                    );
+                  }}
+                </Droppable>
+              </div>
+            );
+          })}
+        </div>
+      </DragDropContext>
     </div>
   );
 }
